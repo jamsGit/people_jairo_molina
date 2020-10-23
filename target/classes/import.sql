e95096fd28ce19bda5b59fc575e0f342ca6cd7b7
Jairo Antonio Molina Salinas <jairounerg@gmail.com>
2020-10-23 17:58:56 -0500

    target

---
+++ target/classes/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO people(document, fingerprint,blacklist) values('6331034', true, false);
+INSERT INTO people(document, fingerprint,blacklist) values('12846613', true, true);
+INSERT INTO people(document, fingerprint,blacklist) values('12345678', false, false);
+INSERT INTO people(document, fingerprint,blacklist) values('87654321', false, false);
